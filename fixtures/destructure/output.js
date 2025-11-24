500713a68fff8446e473d1e7ffb919086d103438
jstaab <jstaab@netflix.com>
2025-11-23 21:54:33 -1000

    feat: remove entire destructuring statement when all elements are unused (#10)
    * introduce failing test for unused destructured object
    
    * feat: remove entire destructuring statement when all elements are unused
    
    Previously, when all elements in a destructuring pattern were unused, we would leave behind an empty destructuring statement like `const { } = utils;`. Now it properly removes the whole thing. And added a test case to verify that behavior.

---
+++ fixtures/destructure/output.js
@@ -0,0 +1,7 @@
+const utils = { foo: 1, bar: 2, baz: 3 };
+
+console.log('All object destructured elements are unused');
+
+const arr = [1, 2, 3];
+
+console.log('All array destructured elements are unused');
